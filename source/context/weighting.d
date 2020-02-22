1b357f36d4a505936b8bda0fea0262918faf7813
jj1lis <rinta.nambuline205@gmail.com>
2020-02-22 18:48:28 +0900

    [fix]:about NaN and infinity value

--- source/context/weighting.d
+++ source/context/weighting.d
@@ -1,31 +1,30 @@
 module context.weighting;
 
 import std.algorithm;
+import std.math;
 import std.conv:to;
 
 real weighting(int weight,const int[] _weightlist)in{
     assert(_weightlist.length!=0);
 }out(result){
-    assert(result!=real.nan);
+    assert(!result.isNaN&&!result.isInfinity);
 }do{
     auto weightlist=_weightlist.dup;
-    enum lankCoeff{
-        first=3.,
-        second=2,
-        third=1.5,
-        Dedault=1.,
-    }
 
     sort!("a>b")(weightlist);
     int lank=int.max;
     foreach(i;0..weightlist.length){
         if(weight>=weightlist[i]){
-            lank=i.to!int;
+            lank=i.to!int+1;
             break;
         }
     }
+    //import std.stdio;
 
-    return correctFunc(lank/weightlist.length);
+    //writefln("lank=%s %s:weightlist.length=%s %s:lank/weightlist.length.to!real=%s %s\ncorrectFunc(lank/weightlist.length.to!real)=%s %s",typeof(lank).stringof,lank,typeof(weightlist.length).stringof,weightlist.length,typeof(lank/weightlist.length.to!real).stringof,lank/weightlist.length.to!real,typeof(correctFunc(lank/weightlist.length.to!real)).stringof,correctFunc(lank/weightlist.length.to!real));
+    return correctFunc(lank/weightlist.length.to!real);
+    //if(result.isNaN||result.isInfinity)assert(0);
+    //return result;
 }
 
 //example
